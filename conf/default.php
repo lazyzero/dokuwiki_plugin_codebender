b0d07cb8842f2a8a855d66001e75fe452cad0df2
Christian Moll <christian.moll@list.lu>
2016-05-19 11:46:14 +0200

    initial commit

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the codebender plugin
+ *
+ * @author Christian Moll <christian@chrmoll.de>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the codebender plugin
+ *
+ * @author Christian Moll <christian@chrmoll.de>
+ */
+
+
+//$meta['fixme'] = array('string');
+
